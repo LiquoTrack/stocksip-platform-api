7bc2f9e04cd83f62daa31eef19009591c1ced813
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-23 19:41:49 -0500

    chore: add account id serializer

--- LiquoTrack.StocksipPlatform.API/Shared/Domain/Model/ValueObjects/AccountId.cs
+++ LiquoTrack.StocksipPlatform.API/Shared/Domain/Model/ValueObjects/AccountId.cs
@@ -1,3 +1,33 @@
 ﻿namespace LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
 
-public record AccountId();
+/// <summary>
+///     This record class serves as a Value Object for an identifier for the Account aggregate.
+/// </summary>
+public record AccountId()
+{
+    /// <summary>
+    /// This is the value of the AccountId Value Object.
+    /// </summary>
+    private string Id { get; } = string.Empty;
+
+    /// <summary>
+    ///     Default constructor for the AccountId Value Object.
+    ///     It is used to validate if the provided id is a non-empty string.
+    /// </summary>
+    /// <param name="id"> The identifier for the account </param>
+    /// <exception cref="ArgumentException">Account ID must be a non-empty string.</exception>
+    public AccountId(string id) : this()
+    {
+        if (id == null || id.Trim().Length == 0)
+        {
+            throw new ArgumentException("Account ID must be a non-empty string.");
+        }
+        
+        Id = id;
+    }
+    
+    /// <summary>
+    ///     This method returns the string representation of the AccountId Value Object.
+    /// </summary>
+    public string GetId => Id;
+}
---
+++ LiquoTrack.StocksipPlatform.API/Shared/Infrastructure/Persistence/MongoDB/Configuration/Serializers/AccountIdSerializer.cs
@@ -0,0 +1,29 @@
+﻿using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+using MongoDB.Bson.Serialization;
+
+namespace LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Persistence.MongoDB.Configuration.Serializers;
+
+/// <summary>
+///     This class provides a custom serializer for the AccountId value object to be used in MongoDB.
+/// </summary>
+public class AccountIdSerializer : IBsonSerializer<AccountId>
+{
+    public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, AccountId value)
+    {
+        context.Writer.WriteString(value.GetId);
+    }
+
+    public AccountId Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    {
+        var value = context.Reader.ReadString();
+        return new AccountId(value);
+    }
+
+    void IBsonSerializer.Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        => Serialize(context, args, (AccountId)value);
+
+    object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        => Deserialize(context, args);
+
+    public Type ValueType => typeof(AccountId);
+}
