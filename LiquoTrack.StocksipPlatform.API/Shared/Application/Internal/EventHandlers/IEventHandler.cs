75aa159a2ed22d0cbc3f9ac676bcc30ed2387155
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-23 20:29:09 -0500

    chore: add event and event handler interfaces

--- LiquoTrack.StocksipPlatform.API/Shared/Application/Internal/EventHandlers/IEventHandler.cs
+++ LiquoTrack.StocksipPlatform.API/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace LiquoTrack.StocksipPlatform.API.Shared.Application.Internal.EventHandlers;
+﻿using Cortex.Mediator.Notifications;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.Events;
 
-public interface IEventHandler
+namespace LiquoTrack.StocksipPlatform.API.Shared.Application.Internal.EventHandlers;
+
+/// <summary>
+///     This class serves as a base interface for all event handlers.
+/// </summary>
+/// <typeparam name="TEvent">
+///     The type of event to handle.
+/// </typeparam>
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
 {
     
 }
