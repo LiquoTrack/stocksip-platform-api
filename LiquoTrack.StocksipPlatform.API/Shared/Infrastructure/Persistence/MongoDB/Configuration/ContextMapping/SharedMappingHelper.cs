b3d25bf8b7496f1486c6e5a794db77b912ccd219
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-23 20:01:58 -0500

    chore: add app db context

---
+++ LiquoTrack.StocksipPlatform.API/Shared/Infrastructure/Persistence/MongoDB/Configuration/ContextMapping/SharedMappingHelper.cs
@@ -0,0 +1,20 @@
+﻿using LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Persistence.MongoDB.Configuration.Serializers;
+
+namespace LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Persistence.MongoDB.Configuration.ContextMapping;
+
+/// <summary>
+///     This class provides helper methods for MongoDB mapping configurations.
+///     It allows the mapping for value objects that are used inside a class.
+/// </summary>
+public static class SharedMappingHelper
+{
+    /// <summary>
+    ///     This method registers all shared MongoDB mappings.
+    ///     It should be called once during the application startup.
+    /// </summary>
+    public static void RegisterSharedMappings()
+    {
+        // Use of AccountId Value Object serializer
+        SerializerRegistrationHelper.TryRegisterSerializer(new AccountIdSerializer());
+    }
+}
