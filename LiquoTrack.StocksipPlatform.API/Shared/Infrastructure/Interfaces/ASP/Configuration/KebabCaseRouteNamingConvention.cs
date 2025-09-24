6a64db6d745afb638b87554c6c00e1dab40d1d18
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-23 19:31:38 -0500

    chore: add route naming convention for endpoints

---
+++ LiquoTrack.StocksipPlatform.API/Shared/Infrastructure/Interfaces/ASP/Configuration/KebabCaseRouteNamingConvention.cs
@@ -0,0 +1,37 @@
+﻿using LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+
+/// <summary>
+///     This convention modifies the route templates of controllers and their actions to use kebab-case naming.
+/// </summary>
+public class KebabCaseRouteNamingConvention : IControllerModelConvention
+{
+    /// <summary>
+    ///     This method replaces the [controller] token in the route template with the kebab-case version of the controller name.
+    /// </summary>
+    private static AttributeRouteModel? ReplaceControllerTemplate(SelectorModel selector, string name)
+    {
+        return selector.AttributeRouteModel != null ? new AttributeRouteModel
+        {
+            Template = selector.AttributeRouteModel.Template?.Replace("[controller]", name.ToKebabCase())
+        } : null;
+    }
+    
+    /// <summary>
+    ///     This method applies the kebab-case naming convention to the controller and its actions.
+    /// </summary>
+    public void Apply(ControllerModel controller)
+    {
+        foreach (var selector in controller.Selectors)
+        {
+            selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
+        }
+
+        foreach (var selector in controller.Actions.SelectMany(a => a.Selectors))
+        {
+            selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
+        }
+    }
+}
