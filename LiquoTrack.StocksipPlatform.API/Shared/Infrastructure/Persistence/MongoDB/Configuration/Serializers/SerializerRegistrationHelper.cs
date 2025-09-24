b4257fc5bd18d96206081482b20a2ec5c76a3dcd
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-23 19:55:51 -0500

    chore: add serializer registration helper

---
+++ LiquoTrack.StocksipPlatform.API/Shared/Infrastructure/Persistence/MongoDB/Configuration/Serializers/SerializerRegistrationHelper.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Persistence.MongoDB.Configuration.Serializers;
+
+/// <summary>
+///     Helper class to register MongoDB serializers safely.
+///     It checks if a serializer is already registered for a type before attempting to register a new
+/// </summary>
+public static class SerializerRegistrationHelper
+{
+    /// <summary>
+    ///     Method to try registering a MongoDB serializer for a specific type T.
+    /// </summary>
+    public static void TryRegisterSerializer<T>(IBsonSerializer<T> serializer)
+    {
+        var type = typeof(T);
+        var registry = BsonSerializer.SerializerRegistry;
+        var currentSerializer = registry.GetSerializer<T>();
+        
+        if (currentSerializer.GetType() == serializer.GetType()) return;
+        
+        try
+        {
+            BsonSerializer.RegisterSerializer(serializer);
+        }
+        catch (BsonSerializationException ex)
+        {
+            if (!ex.Message.Contains("There is already a serializer registered"))
+                throw;
+        }
+    }
+}
