46dcc70d58325b78b93a5c1dbab44a9ed8eb4143
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-23 19:18:44 -0500

    chore: add base entities

---
+++ LiquoTrack.StocksipPlatform.API/Shared/Domain/Model/ValueObjects/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+
+/// <summary>
+///     This interface represents an entity with a unique identifier.
+/// </summary>
+public interface IEntity
+{
+    ObjectId Id { get; }
+}
