474d0a4939f2a4496f6b3896284561c27d8a1168
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-27 17:35:27 -0500

    feat(products): add query for retrieve products for a given supplier id

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Queries/GetAllProductsBySupplierIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Queries;
+
+/// <summary>
+///     Query to get all products for a given supplier.
+/// </summary>
+public record GetAllProductsBySupplierIdQuery(AccountId SupplierId);
