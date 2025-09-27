c331a0508487009274d47dc9edfb23d2af41d417
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-26 20:53:46 -0500

    feat(products): add value objects

--- LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Aggregates/Product.cs
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Aggregates/Product.cs
@@ -1,6 +1,17 @@
-﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Aggregates;
+﻿using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+using MongoDB.Bson;
 
-public class Product
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Aggregates;
+
+/// <summary>
+///     The Product Aggregate Root entity.
+/// </summary>
+public class Product : IEntity
 {
+    /// <summary>
+    ///     The unique identifier of the product.
+    /// </summary>
+    public ObjectId Id { get; } = ObjectId.GenerateNewId();
+    
     
 }
