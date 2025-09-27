f6a5d21fc68a511f3f4326ff92f6df5e1ea9d910
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-27 18:49:30 -0500

    feat(products): add command service implementation for products

--- LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Commands/UpdateProductInformationCommand.cs
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Commands/UpdateProductInformationCommand.cs
@@ -1,5 +1,6 @@
 ﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.ValueObjects;
 using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+using MongoDB.Bson;
 
 namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Commands;
 
@@ -7,6 +8,7 @@ namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Comma
 ///     Command to update product information.
 /// </summary>
 public record UpdateProductInformationCommand(
+        ObjectId ProductId,
         string Name,
         Money UnitPrice,
         ProductMinimumStock MinimumStock,
--- LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Commands/UpdateProductMinimumStockCommand.cs
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Commands/UpdateProductMinimumStockCommand.cs
@@ -1,8 +1,9 @@
 ﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.ValueObjects;
+using MongoDB.Bson;
 
 namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Commands;
 
 /// <summary>
 ///     Command to update the minimum stock of a product.
 /// </summary>
-public record UpdateProductMinimumStockCommand(int NewMinimumStock);
+public record UpdateProductMinimumStockCommand(ObjectId ProductId, int NewMinimumStock);
