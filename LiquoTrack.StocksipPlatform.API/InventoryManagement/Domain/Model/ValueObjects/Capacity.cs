f95d966f8964c67a971c48e9463cc65c5b69a2f7
Far14z <faridce14@gmail.com>
2025-09-28 12:54:20 -0500

    feat(warehouses): add capacity, temperature and address value object.

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/ValueObjects/Capacity.cs
@@ -0,0 +1,41 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+///    Record class that serves as a Value Object for the capacity of a warehouse.
+/// </summary>
+public record Capacity()
+{
+    /// <summary>
+    ///     The value of the capacity.
+    /// </summary>
+    private decimal Value { get; } = 0;
+
+    /// <summary>
+    ///     Default constructor for Capacity.
+    /// </summary>
+    /// <param name="totalCapacity">
+    ///     The total capacity value of the warehouse. Must be a non-negative value and less than or equal to 100,000.
+    /// </param>
+    /// <exception cref="ArgumentException">
+    ///     Thrown when the provided total capacity value is negative or exceeds 100,000.
+    /// </exception>
+    public Capacity(decimal totalCapacity) : this()
+    {
+        if (!IsCapacityValid(totalCapacity))
+            throw new ArgumentException($"{nameof(totalCapacity)} must be greater than {nameof(Capacity)}");
+        
+        Value = totalCapacity;
+    }
+
+    /// <summary>
+    ///     Method to validate the capacity value.
+    /// </summary>
+    /// <param name="totalCapacity">
+    ///     The total capacity value of the warehouse to validate.
+    /// </param>
+    /// <returns>
+    ///     A boolean indicating whether the capacity value is valid (non-negative and less than or equal to 100,000).
+    /// </returns>
+    private static bool IsCapacityValid(decimal totalCapacity) =>
+        totalCapacity is >= 0 and <= 100000;
+}
---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/ValueObjects/Temperature.cs
@@ -0,0 +1,53 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+///     Record class that serves as a Value Object for the temperature range of a warehouse.
+/// </summary>
+public record Temperature()
+{
+    /// <summary>
+    ///     The maximum temperature value of the warehouse.
+    /// </summary>
+    private decimal MinTemperature { get; } = 0;
+    
+    /// <summary>
+    ///     The minimum temperature value of the warehouse.
+    /// </summary>
+    private decimal MaxTemperature { get; } = 0;
+
+    /// <summary>
+    ///     Default constructor for Temperature.
+    /// </summary>
+    /// <param name="minTemperature">
+    ///     The minimum temperature value of the warehouse. Must be less than the maximum temperature.
+    /// </param>
+    /// <param name="maxTemperature">
+    ///     The maximum temperature value of the warehouse. Must be greater than the minimum temperature and less than or equal to 50.
+    /// </param>
+    /// <exception cref="ArgumentException">
+    ///     Thrown when the provided temperature values are invalid.
+    /// </exception>
+    public Temperature(decimal minTemperature, decimal maxTemperature) : this()
+    {
+        if (!IsTemperatureValid(maxTemperature, minTemperature))
+            throw new ArgumentException($"{nameof(maxTemperature)} must be greater than {nameof(minTemperature)} and less than or equal to 50");
+        
+        MinTemperature = minTemperature;
+        MaxTemperature = maxTemperature;
+    }
+
+    /// <summary>
+    ///     Default constructor for Temperature.
+    /// </summary>
+    /// <param name="maxTemperature">
+    ///     Indicates the maximum temperature value of the warehouse. Must be greater than the minimum temperature and less than or equal to 50.
+    /// </param>
+    /// <param name="minTemperature">
+    ///     Indicates the minimum temperature value of the warehouse. Must be less than the maximum temperature.
+    /// </param>
+    /// <returns>
+    ///     A boolean indicating whether the temperature values are valid.
+    /// </returns>
+    private static bool IsTemperatureValid(decimal maxTemperature, decimal minTemperature) =>
+        (maxTemperature > minTemperature) && (maxTemperature <= 50);
+}
