37a30df0639ae081dd268e6e5ec1f5911e0b82c3
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-28 14:17:50 -0500

    feat(warehouses): add related exceptions

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Exceptions/WarehouseFailedUpdateException.cs
@@ -0,0 +1,31 @@
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Aggregates;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Exceptions;
+
+/// <summary>
+///     Exception class for handling Warehouse update failures.
+/// </summary>
+public class WarehouseFailedUpdateException : Exception
+{
+    /// <summary>
+    ///     The name of the Entity that caused the exception.
+    /// </summary>
+    private const string Value = nameof(Warehouse);
+
+    /// <summary>
+    ///     Default constructor that creates a new instance of the WarehouseFailedUpdateException class.
+    /// </summary>
+    public WarehouseFailedUpdateException()
+        : base($"Error trying to update the '{Value}'...")
+    { }
+    
+    /// <summary>
+    ///     Constructor that creates a new instance of the WarehouseFailedUpdateException class with additional details.
+    /// </summary>
+    /// <param name="details">
+    ///     The details about why the Entity is considered invalid.
+    /// </param>
+    public WarehouseFailedUpdateException(string details)
+        : base($"Error trying to update the '{Value}': {details}")
+    { }
+}
