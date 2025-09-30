c36b6213469e7f335308d22a15e323e0912ea528
Far14z <faridce14@gmail.com>
2025-09-30 02:51:22 -0500

    feat(account): add repository interfaces for account, business, plan, and subscription entities.

--- LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Repositories/ISubscriptionsRepository.cs
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Repositories/ISubscriptionsRepository.cs
@@ -1,12 +1,12 @@
 ﻿using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Aggregates;
-using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Commands;
 using LiquoTrack.StocksipPlatform.API.Shared.Domain.Repositories;
 
 namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Repositories;
 
 /// <summary>
-///     Repository interface for managing Account entities.
+///     Repository interface for managing Subscription entities.
 /// </summary>
-public interface IAccountRepository : IBaseRepository<Account>
+public interface ISubscriptionsRepository : IBaseRepository<Subscription>
 {
+    
 }
