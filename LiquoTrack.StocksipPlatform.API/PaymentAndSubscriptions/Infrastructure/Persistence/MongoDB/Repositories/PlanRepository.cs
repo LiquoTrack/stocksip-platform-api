8757b8b32564333355774f220c26dad85944bed2
Far14z <faridce14@gmail.com>
2025-09-30 02:59:16 -0500

    feat(account): implement repository classes for account, business, plan, and subscription entities.

---
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Infrastructure/Persistence/MongoDB/Repositories/PlanRepository.cs
@@ -0,0 +1,14 @@
+﻿using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Entities;
+using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Repositories;
+using LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Persistence.MongoDB.Configuration;
+using LiquoTrack.StocksipPlatform.API.Shared.Infrastructure.Persistence.MongoDB.Repositories;
+
+namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Infrastructure.Persistence.MongoDB.Repositories;
+
+public class PlansRepository(AppDbContext context) : BaseRepository<Plan>(context), IPlanRepository
+{
+    public Task SeedPlansAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
