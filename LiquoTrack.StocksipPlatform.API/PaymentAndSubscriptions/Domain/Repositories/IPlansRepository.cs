07f2f9cd7e9c6cf9ab36e19dc05e8470d07e9444
Far14z <faridce14@gmail.com>
2025-09-30 02:52:32 -0500

    feat(account): add method to seed predefined plans in repository interface.

--- LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Repositories/IPlansRepository.cs
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Repositories/IPlansRepository.cs
@@ -8,5 +8,11 @@ namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Reposit
 /// </summary>
 public interface IPlansRepository : IBaseRepository<Plan>
 {
-    
+    /// <summary>
+    ///     Method to seed predefined plans into the database.
+    /// </summary>
+    /// <returns>
+    ///     A confirmation of the seeding operation.
+    /// </returns>
+    Task SeedPlansAsync();
 }
