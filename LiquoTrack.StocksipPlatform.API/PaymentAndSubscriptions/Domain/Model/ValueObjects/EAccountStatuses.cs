f6a43df029ab2acfa794a72c3c75125096c72564
Far14z <faridce14@gmail.com>
2025-09-29 21:51:21 -0500

    feat(account): add value objects for account roles, business and plans information.

--- LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Model/ValueObjects/EAccountStatuses.cs
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Model/ValueObjects/EAccountStatuses.cs
@@ -1,6 +1,12 @@
 ﻿namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.ValueObjects;
 
+/// <summary>
+///     Value object representing the status of an account.
+/// </summary>
 public enum EAccountStatuses
 {
-    
+    Active,
+    Inactive,
+    Suspended,
+    Closed
 }
---
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Model/ValueObjects/EPaymentFrequency.cs
@@ -0,0 +1,12 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.ValueObjects;
+
+/// <summary>
+///     Value object representing different payment frequencies for subscription plans.
+/// </summary>
+public enum EPaymentFrequency
+{
+    Monthly,
+    Quarterly,
+    SemiAnnual,
+    Yearly
+}
