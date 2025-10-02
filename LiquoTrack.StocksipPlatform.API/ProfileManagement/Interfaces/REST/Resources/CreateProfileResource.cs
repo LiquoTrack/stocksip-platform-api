90f4b88ddce8b4d4b2e067971f28663c4fe3b766
Camila Huamani <jyb990106@gmail.com>
2025-10-02 18:55:01 -0500

    feat(profiles): add profile resources.

---
+++ LiquoTrack.StocksipPlatform.API/ProfileManagement/Interfaces/REST/Resources/CreateProfileResource.cs
@@ -0,0 +1,17 @@
+namespace LiquoTrack.StocksipPlatform.API.ProfileManagement.Interfaces.REST.Resources;
+
+/// <summary>
+/// Resource for creating a new profile.
+/// </summary>
+/// <param name="FirstName">The first name of the person.</param>
+/// <param name="LastName">The last name of the person.</param>
+/// <param name="PhoneNumber">The phone number.</param>
+/// <param name="ProfilePictureUrl">The profile picture URL.</param>
+/// <param name="UserId">The user ID associated with the profile.</param>
+public record CreateProfileResource(
+    string FirstName,
+    string LastName,
+    string PhoneNumber,
+    string ProfilePictureUrl,
+    string UserId
+);
