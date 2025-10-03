db1e5cc8408d842ed004c7eaf279bf8b4ead026e
Far14z <faridce14@gmail.com>
2025-10-02 20:24:05 -0500

    feat(accounts): implement Account and Business command services with necessary constructors and properties.

---
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Application/Internal/CommandServices/BusinessCommandService.cs
@@ -0,0 +1,30 @@
+﻿using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Commands;
+using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Entities;
+using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.ValueObjects;
+using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Repositories;
+using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Services;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Repositories;
+
+namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Application.Internal.CommandServices;
+
+/// <summary>
+///     Method to handle the creation of a new business. 
+/// </summary>
+public class BusinessCommandService(IBusinessRepository businessRepository) : IBusinessCommandService
+{
+    /// <summary>
+    ///     Method to handle the creation of a new business.
+    /// </summary>
+    /// <param name="command">
+    ///     The command containing the details for creating a new business.
+    /// </param>
+    /// <returns>
+    ///     A task representing the asynchronous operation. The task result contains the newly created business.
+    /// </returns>
+    public async Task<Business?> Handle(CreateBusinessCommand command)
+    {
+        var business = new Business(new BusinessName(command.BusinessName));
+        await businessRepository.AddAsync(business);
+        return business;
+    }
+}
---
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Domain/Services/IBusinessCommandService.cs
@@ -0,0 +1,21 @@
+﻿using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Commands;
+using LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Model.Entities;
+
+namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Domain.Services;
+
+/// <summary>
+///     Interface for handling business-related commands.
+/// </summary>
+public interface IBusinessCommandService
+{
+    /// <summary>
+    ///     Method to handle the creation of a new business.  
+    /// </summary>
+    /// <param name="command">
+    ///     The command containing the details for creating a new business. 
+    /// </param>
+    /// <returns>
+    ///     A task representing the asynchronous operation. The task result contains the newly created business.
+    /// </returns>
+    Task<Business?> Handle(CreateBusinessCommand command);
+}
