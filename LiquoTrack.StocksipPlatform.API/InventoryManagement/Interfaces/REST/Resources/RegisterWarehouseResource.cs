7b9cf5c46f59f79630835b2a8d3066b4c09c5ffc
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-10-04 20:04:21 -0500

    feat(warehouses): add resources

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Interfaces/REST/Resources/RegisterWarehouseResource.cs
@@ -0,0 +1,18 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Interfaces.REST.Resources;
+
+/// <summary>
+///     Resource class for registering a new warehouse.
+/// </summary>
+public record RegisterWarehouseResource(
+    string Name,
+    string AddressStreet,
+    string AddressCity,
+    string AddressDistrict,
+    string AddressPostalCode,
+    string AddressCountry,
+    decimal TemperatureMin,
+    decimal TemperatureMax,
+    double Capacity,
+    string ImageUrl,
+    string AccountId
+    );
