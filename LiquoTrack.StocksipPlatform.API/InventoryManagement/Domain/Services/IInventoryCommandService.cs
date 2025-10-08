73e4dbb1cd6e2bec4002c2b4b81b02fff7074e68
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-10-07 20:43:34 -0500

    feat(inventories): add services interfaces

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Services/IInventoryCommandService.cs
@@ -0,0 +1,32 @@
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Aggregates;
+using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Commands;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Services;
+
+/// <summary>
+///     Interface for handling inventory-related commands.
+/// </summary>
+public interface IInventoryCommandService
+{
+    /// <summary>
+    ///     Method to handle the addition of products to a warehouse.
+    /// </summary>
+    /// <param name="command">
+    ///     The command containing the details for adding products to a warehouse.
+    /// </param>
+    /// <returns>
+    ///     An inventory object representing the updated inventory.
+    /// </returns>
+    Task<Inventory?> Handle(AddProductsToWarehouseCommand command);
+
+    /// <summary>
+    ///     Method to handle the removal of products from a warehouse.   
+    /// </summary>
+    /// <param name="command">
+    ///     The command containing the details for removing products from a warehouse. 
+    /// </param>
+    /// <returns>
+    ///     The updated inventory.
+    /// </returns>
+    Task<Inventory?> Handle(DecreaseProductsFromWarehouseCommand command);
+}
---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Services/IInventoryQueryService.cs
@@ -0,0 +1,43 @@
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Aggregates;
+using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Queries;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Services;
+
+/// <summary>
+///     Interface for handling inventory-related queries.
+/// </summary>
+public interface IInventoryQueryService
+{
+    /// <summary>
+    ///     Method to handle the retrieval of all inventories associated with a specific product ID.
+    /// </summary>
+    /// <param name="query">
+    ///     The query object containing the product ID for which inventories are to be retrieved.
+    /// </param>
+    /// <returns>
+    ///     A list of inventories associated with the specified product ID.
+    /// </returns>
+    Task<IEnumerable<Inventory>> Handle(GetAllInventoriesByProductIdQuery query);
+    
+    /// <summary>
+    ///     The method to handle the retrieval of all inventories associated with a specific warehouse ID.   
+    /// </summary>
+    /// <param name="query">
+    ///     The query object containing the warehouse ID for which inventories are to be retrieved.
+    /// </param>
+    /// <returns>
+    ///     A list of inventories associated with the specified warehouse ID.
+    /// </returns>
+    Task<IEnumerable<Inventory>> Handle(GetAllInventoriesByWarehouseIdQuery query);
+    
+    /// <summary>
+    ///     The method to handle the retrieval of an inventory by product ID, warehouse ID, and expiration date. 
+    /// </summary>
+    /// <param name="query">
+    ///     The query object containing the product ID, warehouse ID, and expiration date for which an inventory is to be retrieved.
+    /// </param>
+    /// <returns>
+    ///     The inventory if found; otherwise, null.
+    /// </returns>
+    Task<Inventory?> Handle(GetInventoryByProductIdWarehouseIdAndExpirationDateQuery query);
+}
