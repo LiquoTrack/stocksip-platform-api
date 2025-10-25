2907ac0cb61918dcc5637b6bff9d88f65f95cd24
Camila Huamani <jyb990106@gmail.com>
2025-10-24 21:35:31 -0500

    feat(purchase-order): add purchase order commands.

---
+++ LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Domain/Model/Commands/CreatePurchaseOrderCommand.cs
@@ -0,0 +1,9 @@
+namespace LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Domain.Model.Commands;
+
+/// <summary>
+/// Command to create a new purchase order.
+/// </summary>
+/// <param name="orderCode">The unique code for the purchase order.</param>
+/// <param name="catalogIdBuyFrom">The identifier of the catalog to buy from.</param>
+/// <param name="buyer">The account identifier of the buyer.</param>
+public record CreatePurchaseOrderCommand(string orderCode, string catalogIdBuyFrom, string buyer);
