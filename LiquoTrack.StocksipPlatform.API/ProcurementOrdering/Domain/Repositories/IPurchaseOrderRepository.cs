7e7ab92652114bf04e2e033715cc275921f5ccaa
Camila Huamani <jyb990106@gmail.com>
2025-10-26 22:03:06 -0500

    feat(purchase-order): update purchase orders assemblers.

--- LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Domain/Repositories/IPurchaseOrderRepository.cs
+++ LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Domain/Repositories/IPurchaseOrderRepository.cs
@@ -1,12 +1,13 @@
 ﻿using LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Domain.Model.Aggregates;
 using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Repositories;
 
 namespace LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Domain.Repositories;
 
 /// <summary>
 /// Repository interface for purchase order operations.
 /// </summary>
-public interface IPurchaseOrderRepository
+public interface IPurchaseOrderRepository : IBaseRepository<PurchaseOrder>
 {
     /// <summary>
     /// Gets a purchase order by its identifier.
@@ -23,43 +24,23 @@ public interface IPurchaseOrderRepository
     Task<PurchaseOrder?> GetByOrderCodeAsync(string orderCode);
 
     /// <summary>
-    /// Gets all purchase orders.
-    /// </summary>
-    /// <returns>A collection of all purchase orders.</returns>
-    Task<IEnumerable<PurchaseOrder>> GetAllAsync();
-
-    /// <summary>
     /// Gets all purchase orders for a specific buyer.
     /// </summary>
     /// <param name="buyer">The buyer account identifier.</param>
     /// <returns>A collection of purchase orders for the buyer.</returns>
-    Task<IEnumerable<PurchaseOrder>> GetByBuyerAsync(AccountId buyer);
+    Task<IEnumerable<PurchaseOrder>> FindByBuyerAsync(AccountId buyer);
 
     /// <summary>
     /// Gets all purchase orders from a specific catalog.
     /// </summary>
     /// <param name="catalogId">The catalog identifier.</param>
     /// <returns>A collection of purchase orders from the catalog.</returns>
-    Task<IEnumerable<PurchaseOrder>> GetByCatalogAsync(CatalogId catalogId);
+    Task<IEnumerable<PurchaseOrder>> FindByCatalogAsync(CatalogId catalogId);
 
     /// <summary>
-    /// Creates a new purchase order.
+    /// Checks if a purchase order exists by its order code.
     /// </summary>
-    /// <param name="order">The purchase order to create.</param>
-    /// <returns>The created purchase order.</returns>
-    Task<PurchaseOrder> CreateAsync(PurchaseOrder order);
-
-    /// <summary>
-    /// Updates an existing purchase order.
-    /// </summary>
-    /// <param name="order">The purchase order to update.</param>
-    /// <returns>True if the update was successful, otherwise false.</returns>
-    Task<bool> UpdateAsync(PurchaseOrder order);
-
-    /// <summary>
-    /// Deletes a purchase order.
-    /// </summary>
-    /// <param name="id">The purchase order identifier.</param>
-    /// <returns>True if the deletion was successful, otherwise false.</returns>
-    Task<bool> DeleteAsync(PurchaseOrderId id);
+    /// <param name="orderCode">The order code to check.</param>
+    /// <returns>True if the order exists, otherwise false.</returns>
+    Task<bool> ExistsByOrderCodeAsync(string orderCode);
 }
---
+++ LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Interfaces/REST/Assemblers/CreatePurchaseOrderCommandFromResourceAssembler.cs
@@ -0,0 +1,24 @@
+using LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Domain.Model.Commands;
+using LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Interfaces.REST.Resources;
+
+namespace LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Interfaces.REST.Assemblers;
+
+/// <summary>
+/// Assembler to convert CreatePurchaseOrderResource to CreatePurchaseOrderCommand.
+/// </summary>
+public static class CreatePurchaseOrderCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Converts a CreatePurchaseOrderResource to a CreatePurchaseOrderCommand.
+    /// </summary>
+    /// <param name="resource">The creation purchase order resource.</param>
+    /// <returns>The create purchase order command.</returns>
+    public static CreatePurchaseOrderCommand ToCommandFromResource(CreatePurchaseOrderResource resource)
+    {
+        return new CreatePurchaseOrderCommand(
+            resource.orderCode,
+            resource.catalogIdBuyFrom,
+            resource.buyer
+        );
+    }
+}
