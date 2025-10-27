f215862f25da09ee042fc06807cef6c644e15bb2
Camila Huamani <jyb990106@gmail.com>
2025-10-26 22:00:16 -0500

    feat(catalog): update catalog resources.

--- LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Domain/Model/Entities/CatalogItem.cs
+++ LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Domain/Model/Entities/CatalogItem.cs
@@ -1,20 +1,31 @@
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.Entities;
 using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Domain.Model.Entities;
 
-public record CatalogItem
+/// <summary>
+/// Entity representing an item within a catalog.
+/// </summary>
+public class CatalogItem : Entity
 {
-    public string ProductId { get; init; }
-    public Money UnitPrice { get; init; }
-    public DateTime AddedDate { get; init; }
+    public ProductId ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public Money UnitPrice { get; private set; }
+    public string ImageUrl { get; private set; }
+    public DateTime AddedAt { get; private set; }
 
-    public CatalogItem(string productId, Money unitPrice, DateTime addedDate)
+    public CatalogItem(ProductId productId, string productName, Money unitPrice, string imageUrl, DateTime addedAt)
     {
-        if (string.IsNullOrWhiteSpace(productId))
-            throw new ArgumentException("ProductId cannot be empty", nameof(productId));
-
-        ProductId = productId;
+        ProductId = productId ?? throw new ArgumentNullException(nameof(productId));
+        ProductName = !string.IsNullOrWhiteSpace(productName)
+            ? productName
+            : throw new ArgumentException("Product name cannot be null or empty.", nameof(productName));
         UnitPrice = unitPrice ?? throw new ArgumentNullException(nameof(unitPrice));
-        AddedDate = addedDate;
+        ImageUrl = imageUrl;
+        AddedAt = addedAt;
     }
+
+    // Protected constructor for MongoDB
+    protected CatalogItem() { }
 }
---
+++ LiquoTrack.StocksipPlatform.API/ProcurementOrdering/Interfaces/REST/Resources/CatalogItemResource.cs
@@ -0,0 +1,10 @@
+namespace LiquoTrack.StocksipPlatform.API.ProcurementOrdering.Interfaces.REST.Resources;
+
+public record CatalogItemResource(
+    string ProductId,
+    decimal Amount,
+    string Currency,
+    DateTime AddedDate,
+    string? ProductName,
+    string? ProductImage
+);
