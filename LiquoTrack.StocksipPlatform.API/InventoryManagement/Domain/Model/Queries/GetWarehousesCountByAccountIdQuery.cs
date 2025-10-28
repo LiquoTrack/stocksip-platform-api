52cfe0738947855cadf8242acf6145a2020c59b4
Far14z <faridce14@gmail.com>
2025-10-28 09:32:44 -0500

    feat(warehouses): refactor warehouse retrieval to include total count.

--- LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Queries/GetWarehousesCountByAccountIdQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
-
-namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Queries;
-
-/// <summary>
-///     Record representing a query to get the count of warehouses by AccountId.
-/// </summary>
-public record GetWarehousesCountByAccountIdQuery(AccountId AccountId);
