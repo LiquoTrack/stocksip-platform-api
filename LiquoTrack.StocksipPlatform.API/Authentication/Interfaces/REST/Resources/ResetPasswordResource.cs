cefd70b69deba97e6406c231851578f044d895cb
Far14z <faridce14@gmail.com>
2025-11-07 10:09:47 -0500

    feat(recover-password): add command, resource and assembler to update the password.

---
+++ LiquoTrack.StocksipPlatform.API/Authentication/Interfaces/REST/Resources/ResetPasswordResource.cs
@@ -0,0 +1,6 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.Authentication.Interfaces.REST.Resources;
+
+/// <summary>
+///     Resource for resetting a user's password.'
+/// </summary>
+public record ResetPasswordResource(string Email, string NewPassword);
