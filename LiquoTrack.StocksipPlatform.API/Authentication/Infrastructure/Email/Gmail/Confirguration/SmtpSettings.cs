a425558fea8ee21ca665967c49e56c75211b4799
Far14z <faridce14@gmail.com>
2025-11-07 07:54:21 -0500

    feat(recover-password): add smtp service settings and dependency injection.

---
+++ LiquoTrack.StocksipPlatform.API/Authentication/Infrastructure/Email/Gmail/Confirguration/SmtpSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.Authentication.Infrastructure.Email.Gmail.Confirguration;
+
+/**
+ *      Configurations for the SMTP server.
+ */
+public class SmtpSettings
+{
+    public string Server { get; set; } = null;
+    
+    public int Port { get; set; }
+    
+    public string SenderName { get; set; } = null;
+    
+    public string SenderEmail { get; set; } = null;
+    
+    public string UserName { get; set; } = null;
+    
+    public string Password { get; set; } = null;
+
+    public bool EnableSsl { get; set; } = false;
+}
