0edf822e44ee877e8a3178e2798927640bfc50e4
Far14z <faridce14@gmail.com>
2025-11-07 07:55:22 -0500

    refactor(recover-password): convert user entity into a partial class to add attributes and methods to recover a password.

---
+++ LiquoTrack.StocksipPlatform.API/Authentication/Domain/Model/Aggregates/UserPasswordRecovery.cs
@@ -0,0 +1,53 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.Authentication.Domain.Model.Aggregates;
+
+public partial class User
+{
+    /// <summary>
+    ///     The recovery password of the gmail service.
+    /// </summary>
+    public string? RecoveryPassword { get; set; }
+    
+    /// <summary>
+    ///     Date when the recovery password expires.
+    /// </summary>
+    public DateTime? RecoveryPasswordExpiration { get; set; }
+
+    /// <summary>
+    ///     Method to set the recovery password.
+    /// </summary>
+    /// <param name="recoveryCode">
+    ///     The 6-digit recovery code.
+    /// </param>
+    /// <param name="expiration">
+    ///     The expiration time of the recovery code.
+    /// </param>
+    public void SetRecoveryCode(string recoveryCode, TimeSpan expiration)
+    {
+        RecoveryPassword = recoveryCode;
+        RecoveryPasswordExpiration = DateTime.UtcNow.Add(expiration);
+    }
+
+    /// <summary>
+    ///     Method to check if the recovery code is valid.
+    /// </summary>
+    /// <param name="code">
+    ///     The recovery code to check.
+    /// </param>
+    /// <returns>
+    ///     A boolean indicating whether the recovery code is valid.
+    /// </returns>
+    public bool IsRecoveryCodeValid(string code)
+    {
+        return RecoveryPassword == code && RecoveryPasswordExpiration > DateTime.UtcNow;
+    }
+
+    /// <summary>
+    ///     Method to clear the recovery code.
+    /// </summary>
+    public void ClearRecoveryCode()
+    {
+        RecoveryPassword = null;
+        RecoveryPasswordExpiration = null;   
+    }
+        
+}
