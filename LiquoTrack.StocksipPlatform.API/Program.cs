022d3904cb95028107abcb9e512db9e3075e6a9e
Camila Huamani <jyb990106@gmail.com>
2025-11-07 17:37:50 -0500

    fix: delete unused files.

--- LiquoTrack.StocksipPlatform.API/Program.cs
+++ LiquoTrack.StocksipPlatform.API/Program.cs
@@ -321,7 +321,6 @@ builder.Services.AddScoped<ICatalogCommandService, CatalogCommandService>();
 builder.Services.AddScoped<ICatalogQueryService, CatalogQueryService>();
 
 builder.Services.AddScoped<IProductContextFacade, ProductContextFacade>();
-builder.Services.AddScoped<IAccountContextFacade, AccountContextFacade>();
 
 // Purchase Order converters
 builder.Services.Configure<JsonOptions>(options =>
