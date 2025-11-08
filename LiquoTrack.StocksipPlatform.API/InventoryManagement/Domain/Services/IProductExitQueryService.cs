f0a3008a20a4ff32bb0cd83f7c1bfa1bd8008d91
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-08 00:06:31 -0500

    feat(product-exits): add product exit query service interface

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Services/IProductExitQueryService.cs
@@ -0,0 +1,32 @@
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Entities;
+using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Queries;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Services;
+
+/// <summary>
+///     Interface for handling product exit-related queries.
+/// </summary>
+public interface IProductExitQueryService
+{
+    /// <summary>
+    ///     Handler for the retrieval of all product exits for a given account ID.
+    /// </summary>
+    /// <param name="query">
+    ///     The query object containing the account ID for which product exits are to be retrieved.
+    /// </param>
+    /// <returns>
+    ///     A list of product exits for the specified account ID, if any; otherwise, an empty list.   
+    /// </returns>
+    Task<IEnumerable<ProductExit>> Handle(GetAllProductExitsByAccountIdQuery query);
+    
+    /// <summary>
+    ///     Handler for the retrieval of all product exits for a given warehouse ID.
+    /// </summary>
+    /// <param name="query">
+    ///     The query object containing the warehouse ID for which product exits are to be retrieved.
+    /// </param>
+    /// <returns>
+    ///     A list of product exits for the specified warehouse ID, if any; otherwise, an empty list.  
+    /// </returns>
+    Task<IEnumerable<ProductExit>> Handle(GetAllProductExitsByWarehouseIdQuery query);
+}
