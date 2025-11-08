1c8c10169800d9e7beb18bf43b70d63971057a4e
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-07 23:55:40 -0500

    feat(product-exits): add product exit entity

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Entities/ProductExit.cs
@@ -0,0 +1,41 @@
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.ValueObjects;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Entities;
+
+/// <summary>
+///     A entity representing a product exit from inventory.
+/// </summary>
+/// <param name="inventoryAffectedId">
+///     The identifier of the inventory affected by the product exit.
+/// </param>
+/// <param name="exitReason">
+///     The reason for the product exit.
+/// </param>
+/// <param name="outputQuantity">
+///     The quantity of the product that was outputted.
+/// </param>
+public class ProductExit(
+    ObjectId inventoryAffectedId, 
+    EProductExitReasons exitReason, 
+    int outputQuantity)
+    : Entity
+{
+    /// <summary>
+    /// The identifier of the inventory affected by the product exit.
+    /// </summary>
+    public ObjectId InventoryAffectedId { get; private set; } = inventoryAffectedId;
+
+    /// <summary>
+    /// The reason for the product exit.
+    /// </summary>
+    [BsonRepresentation(BsonType.String)]
+    public EProductExitReasons ExitReason { get; private set; } = exitReason;
+
+    /// <summary>
+    /// The quantity of the product that was outputted.
+    /// </summary>
+    public int OutputQuantity { get; private set; } = outputQuantity;
+}
