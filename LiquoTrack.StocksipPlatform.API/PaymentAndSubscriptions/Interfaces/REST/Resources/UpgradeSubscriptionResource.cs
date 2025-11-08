2974e01fcc368b6ed57c050128a63aabf8d7ce4d
Far14z <faridce14@gmail.com>
2025-11-07 19:05:38 -0500

    feat(subscriptions): add resource and assembler for the upgrade subscription.

---
+++ LiquoTrack.StocksipPlatform.API/PaymentAndSubscriptions/Interfaces/REST/Resources/UpgradeSubscriptionResource.cs
@@ -0,0 +1,6 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.PaymentAndSubscriptions.Interfaces.REST.Resources;
+
+/// <summary>
+///     Command resource for upgrading a subscription.
+/// </summary>
+public record UpgradeSubscriptionResource(string NewPlanId);
