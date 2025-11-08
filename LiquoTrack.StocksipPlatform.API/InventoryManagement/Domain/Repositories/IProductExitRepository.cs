4a609d82e23fe2eb48091320e9fcb1ae619b5645
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-08 00:04:31 -0500

    feat(product-exits): add interface for product exit repository

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Repositories/IProductExitRepository.cs
@@ -0,0 +1,34 @@
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Entities;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Repositories;
+using MongoDB.Bson;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Repositories;
+
+/// <summary>
+///     Interface for managing ProductExit entities.
+/// </summary>
+public interface IProductExitRepository : IBaseRepository<ProductExit>
+{
+    /// <summary>
+    ///     Retrieves all product exits for a given account ID.
+    /// </summary>
+    /// <param name="accountId">
+    ///     The ID of the account to retrieve product exits for.
+    /// </param>
+    /// <returns>
+    ///     A list of product exits for the specified account or a blank list if no product exits are found.
+    /// </returns>
+    Task<IEnumerable<ProductExit>> GetAllByAccountIdAsync(AccountId accountId);
+    
+    /// <summary>
+    ///     Retrieves all product exits for a given warehouse ID.
+    /// </summary>
+    /// <param name="warehouseId">
+    ///     The ID of the warehouse to retrieve product exits for.
+    /// </param>
+    /// <returns>
+    ///     A list of product exits for the specified warehouse or a blank list if no product exits are found.
+    /// </returns>
+    Task<IEnumerable<ProductExit>> GetAllByWarehouseIdAsync(ObjectId warehouseId);
+}
