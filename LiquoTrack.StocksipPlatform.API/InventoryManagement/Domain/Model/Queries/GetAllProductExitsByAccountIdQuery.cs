ce50defc9055c28bf0dfa164ed39550769d4ab41
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-08 00:01:27 -0500

    feat(product-exits): add queries related to product exits

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Queries/GetAllProductExitsByAccountIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using LiquoTrack.StocksipPlatform.API.Shared.Domain.Model.ValueObjects;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Queries;
+
+/// <summary>
+///     Query to retrieve all the product exit history for a specific account.
+/// </summary>
+public record GetAllProductExitsByAccountIdQuery(AccountId AccountId);
