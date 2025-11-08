7d54a0107e7e1445f14aff9bba9ca383e0eb36ee
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-07 23:49:21 -0500

    feat(product-exits): add enum for multiple reasons of a product exit

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/ValueObjects/EProductExitReasons.cs
@@ -0,0 +1,23 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.ValueObjects;
+
+/// <summary>
+/// This enum defines the possible reasons for the exit of a product. This is used to track the reason of the exit of a product.
+/// The possible states are:
+/// - SOLD: When the product is sold.
+/// - DONATED: When the product is donated.
+/// - EXPIRED: When the product is lost because of the expiration date has passed.
+/// - SPOILED: When the product is lost because of the conditions it was stored.
+/// - DAMAGED: When the product packaging is damaged and can no longer be sold.
+/// - BROKE: When the product fell and broke.
+/// - CONSUMED: When a product is consumed internally by the owner of the liquor store.
+/// </summary>
+public enum EProductExitReasons
+{
+    Sold,
+    Donated,
+    Expired,
+    Spoiled,
+    Damaged,
+    Broke,
+    Consumed
+}
