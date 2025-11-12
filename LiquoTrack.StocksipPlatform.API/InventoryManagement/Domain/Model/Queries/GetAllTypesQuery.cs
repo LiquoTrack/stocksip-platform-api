9bbfb34663b16f297bb6c8a780c2e553e3717f62
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-12 13:10:57 -0500

    feat(product-types): add query to retrieve all types

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Model/Queries/GetAllTypesQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Queries;
+
+/// <summary>
+///     Query to get all product types.
+/// </summary>
+public record GetAllTypesQuery();
