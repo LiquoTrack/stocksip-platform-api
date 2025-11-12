3851e0f77bfee92aab9425af4eb7db875ed1299a
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-12 13:21:11 -0500

    feat(product-types): implement methods of types' query service

--- LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Repositories/ITypeRepository.cs
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Repositories/ITypeRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Repositories;
+﻿using LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Model.Entities;
+using LiquoTrack.StocksipPlatform.API.Shared.Domain.Repositories;
+
+namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Repositories;
 
 /// <summary>
 ///     Interface for managing ProductType entities.
 /// </summary>
-public interface ITypeRepository
+public interface ITypeRepository : IBaseRepository<ProductType>
 {
     /// <summary>
     ///     Method to seed the product types in the database.
