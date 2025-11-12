f731648b348895dd0b57bbf8f4649c182986e3d2
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-12 13:12:18 -0500

    feat(product-types): add repository for types entities

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Domain/Repositories/ITypeRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Domain.Repositories;
+
+/// <summary>
+///     Interface for managing ProductType entities.
+/// </summary>
+public interface ITypeRepository
+{
+    /// <summary>
+    ///     Method to seed the product types in the database.
+    /// </summary>
+    /// <returns>
+    ///     A confirmation of the seeding operation.
+    /// </returns>
+    Task SeedTypesNamesAsync();
+}
