26f5beadd6a7b037521a761d429a3e9d78a1ade0
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-16 13:48:03 -0500

    feat(product-transfers): add resource

---
+++ LiquoTrack.StocksipPlatform.API/InventoryManagement/Interfaces/REST/Resources/ProductTransferResource.cs
@@ -0,0 +1,19 @@
+﻿namespace LiquoTrack.StocksipPlatform.API.InventoryManagement.Interfaces.REST.Resources;
+
+/// <summary>
+///     Resource for a product transfer.
+/// </summary>
+public record ProductTransferResource(
+    string TransferId,
+    string ProductId, 
+    string ProductName, 
+    string OriginWarehouseId, 
+    string OriginWarehouseName,
+    string DestinationWarehouseId,
+    string DestinationWarehouseName,
+    int TransferredStock, 
+    int OriginWarehouseRemainingStock,
+    int DestinationWarehouseResultingStock,
+    DateTime TransferDate,
+    string? ExpirationDate
+);
